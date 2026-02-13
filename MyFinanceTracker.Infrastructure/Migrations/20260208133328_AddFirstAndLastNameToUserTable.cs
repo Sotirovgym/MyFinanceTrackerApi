60726dde2a038d1af89a02fb054407d67029c6e4
Georgi Sotirov <sotirovgym@abv.bg>
2026-02-13 09:55:24 +0200

    Add First and Last name to the users table.

---
+++ MyFinanceTracker.Infrastructure/Migrations/20260208133328_AddFirstAndLastNameToUserTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFinanceTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstAndLastNameToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers");
+        }
+    }
+}
