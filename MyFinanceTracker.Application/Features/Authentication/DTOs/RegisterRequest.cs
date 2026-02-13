3582c0447e8116328cc1c28e0bae35f352cc3bab
Georgi Sotirov <sotirovgym@abv.bg>
2026-02-13 10:08:03 +0200

    Add authentication DTOs

---
+++ MyFinanceTracker.Application/Features/Authentication/DTOs/RegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFinanceTracker.Application.Features.Authentication.DTOs
+{
+    public class RegisterRequest
+    {
+        public string Email { get; init; } = null!;
+        public string Password { get; init; } = null!;
+        public string ConfirmPassword { get; init; } = null!;
+        public bool EnableNotifications { get; set; }
+    }
+}
